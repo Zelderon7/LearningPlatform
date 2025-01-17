9bd162a52731a70c1e89d52525db416733e5ad6a
Zelderon7 <zelderon038@gmail.com>
2025-01-17 09:42:53 +0200

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Models.Entities
+{
+    public class User: IdentityUser
+    {
+        public string Role { get; set; } = "STUDENT";
+        public string PictureUrl { get; set; } = "";
+    }
+}
