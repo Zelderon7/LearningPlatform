376b6ea32d9693dd194ab2264afd4a7d731fcd6a
Zelderon7 <zelderon038@gmail.com>
2025-02-09 17:33:23 +0200

    Added coding Task entity

---
+++ WebApplication1/Models/Entities/CodingTask.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models.Entities
+{
+    public class CodingTask
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+        public string Language { get; set; }
+
+        //Navigation properties
+        public User Author { get; set; }
+        
+    }
+}
