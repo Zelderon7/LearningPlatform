024e937f4d4a537d3c4e3f07fd2d9cebbe19d762
Zelderon7 <zelderon038@gmail.com>
2025-02-10 11:35:48 +0200

    Added directoryService

---
+++ WebApplication1/AppConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1
+{
+    public static class AppConstants
+    {
+        public const string CodingTasksDir = "D:\\gluposti\\School\\FinalYearProject\\LearningPlatform\\WebApplication1\\WebApplication1\\Services\\CodeExecutions\\Tasks\\";
+
+        public const string LanguageTemplatesDir = "D:\\gluposti\\School\\FinalYearProject\\LearningPlatform\\WebApplication1\\WebApplication1\\Services\\CodeExecutions\\LanguagesTemplates";
+    }
+}
---
+++ WebApplication1/Services/DirectoryService.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebApplication1.Services
+{
+    public class DirectoryService
+    {
+        internal async Task InitializeTaskFolder(string folderDir, string language)
+        {
+            if (Directory.Exists(folderDir))
+            {
+                throw new Exception("Directory already exists");
+            }
+
+            Directory.CreateDirectory(folderDir);
+            string templatePath = Path.Combine(AppConstants.LanguageTemplatesDir, language.ToLower());
+
+            foreach(string file in Directory.EnumerateFiles(templatePath))
+            {
+                File.Copy(file, Path.Combine(folderDir, Path.GetFileName(file)), false);
+            }
+        }
+    }
+}
